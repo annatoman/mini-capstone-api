4611a1f93ffa3da8596f214618d7beeec33a6898
Anna <annamtoman@gmail.com>
2023-03-01 14:30:14 -0600

    adds single id search

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
   get "/all_products", controller: "products", action: "all_products"
+
+  get "/one_product/:id", controller: "products", action: "one_product"
 end
