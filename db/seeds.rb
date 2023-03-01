43cf542150672076014ab8f35a769d0ee0d8e25e
Anna <annamtoman@gmail.com>
2023-03-01 12:44:22 -0600

    adds products

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+product = Product.new(name: "Delphinium", price: 4, image_url: "example.url", description: "A beautiful and tall blue flower")
+product.save
+
+product = Product.new(name: "Peony", price: 6, image_url: "example.url", description: "A large bloom that comes in a variety of colors")
+product.save
+
+product = Product.new(name: "Chamomile", price: 3, image_url: "example.url", description: "Like a mini-daisy, super cute!")
+product.save
