86b94a217981b67d0c66c1505d2d8dd05e6a7067
Anna <annamtoman@gmail.com>
2023-03-02 14:11:25 -0600

    adds create action

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -8,4 +8,15 @@ class ProductsController < ApplicationController
         @product = Product.find_by(id: params[:id])
         render :show
     end
+
+    def create
+        @product = Product.create(
+            name: "Ranunculus",
+            price: 3,
+            image_url: "test.jpg",
+            description: "A beautiful and delicate flower that comes in a variety of colors"
+        )
+        render :show
+    end
+
 end
